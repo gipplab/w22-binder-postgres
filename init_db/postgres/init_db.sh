e82f0f188f937722280bd70ea68b6362cc129a44
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-12-08 16:48:32 +0100

    Simplify db creation
    Use defaults for user and password

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -4,9 +4,10 @@ set -eux
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Demo PostgreSQL Database initialisation
-psql postgres -c "CREATE USER testuser PASSWORD 'testpass'"
+# psql postgres -c "CREATE USER testuser PASSWORD 'testpass'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
-createdb -O testuser testdb
-
-psql -d testdb -U testuser -f $THISDIR/01_schema.sql
-psql -d testdb -U testuser -f $THISDIR/02_inserts.sql
+# createdb -O testuser testdb
+# creates a db with the default name (name of current user)
+createdb
+psql -f $THISDIR/01_schema.sql
+psql -f $THISDIR/02_inserts.sql
